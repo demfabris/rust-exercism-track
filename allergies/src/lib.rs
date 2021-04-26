09e486ff025f0702827c3479d43c81ea6684748e
Fabricio Dematte <fabriciodematte7p@gmail.com>
2021-04-26 18:25:40 -0300

    allergens

--- allergies/src/lib.rs
+++ allergies/src/lib.rs
@@ -21,23 +21,33 @@ impl Allergies {
 
     pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
         let allergen_score = allergen.to_owned() as u32;
-        self.score >= allergen_score
+        self.score >= 2u32.pow(allergen_score)
     }
 
     pub fn allergies(&self) -> Vec<Allergen> {
-        let score = self.score;
-        let possible_allergens: Vec<Allergen> = vec![
-            Allergen::Eggs,
-            Allergen::Peanuts,
-            Allergen::Shellfish,
-            Allergen::Strawberries,
-            Allergen::Tomatoes,
-            Allergen::Chocolate,
-            Allergen::Pollen,
-            Allergen::Cats,
-        ]
-        .into_iter()
-        .filter(|x| x.to_owned() as u32 <= score)
-        .collect();
+        let mut result: Vec<Allergen> = vec![];
+        let score_as_bits: Vec<u32> = format!("{:b}", self.score)
+            .chars()
+            .rev()
+            .map(|b| b.to_digit(2).unwrap())
+            .collect();
+
+        for (index, bit) in score_as_bits.iter().enumerate() {
+            if *bit == 1 {
+                match index {
+                    0 => result.push(Allergen::Eggs),
+                    1 => result.push(Allergen::Peanuts),
+                    2 => result.push(Allergen::Shellfish),
+                    3 => result.push(Allergen::Strawberries),
+                    4 => result.push(Allergen::Tomatoes),
+                    5 => result.push(Allergen::Chocolate),
+                    6 => result.push(Allergen::Pollen),
+                    7 => result.push(Allergen::Cats),
+                    _ => (),
+                }
+            }
+        }
+
+        result
     }
 }
